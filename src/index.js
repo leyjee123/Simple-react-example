14e569a463df919e197cd198aa8c1d7bc92c3d8b
leyjee123 <76607146+leyjee123@users.noreply.github.com>
2023-01-15 20:59:48 +0800

    cards added

--- src/index.js
+++ src/index.js
@@ -1,30 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import SingleComment from "./SingleComment";
+import UserCard from "./UserCard";
 
 const App = () => {
   return (
     <div>
-      <h1 className='title'>Dismord</h1>
+      <h1 className="title">Dismord</h1>
       <div className="ui comments">
-        <SingleComment
-          name="Carl Adams"
-          date="Today at 5:00PM"
-          comment="i heard he died from ligma?"
-          pic="https://randomuser.me/api/portraits/men/15.jpg"
-        />
-        <SingleComment
-          name="Catherine Fernandez"
-          date="Today at 6:00PM"
-          comment="what is ligma"
-          pic="https://randomuser.me/api/portraits/women/15.jpg"
-        />
-        <SingleComment
-          name="Benjamin Garaham"
-          date="Today at 7:00PM"
-          comment="joe mama"
-          pic="https://randomuser.me/api/portraits/men/51.jpg"
-        />
+        <UserCard>
+          <SingleComment
+            name="Carl Adams"
+            date="Today at 5:00PM"
+            comment="i heard he died from ligma?"
+            pic="https://randomuser.me/api/portraits/men/15.jpg"
+          />
+        </UserCard>
+        <UserCard>
+          <SingleComment
+            name="Catherine Fernandez"
+            date="Today at 6:00PM"
+            comment="what is ligma"
+            pic="https://randomuser.me/api/portraits/women/15.jpg"
+          />
+        </UserCard>
+        <UserCard>
+          <SingleComment
+            name="Benjamin Garaham"
+            date="Today at 7:00PM"
+            comment="joe mama"
+            pic="https://randomuser.me/api/portraits/men/51.jpg"
+          />
+        </UserCard>
       </div>
     </div>
   );
